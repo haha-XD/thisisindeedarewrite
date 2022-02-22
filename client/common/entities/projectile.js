5c6d1ba0340b6ee6f9de98e58d65394f23a9f8c0
richard herndon <richardherndon3469@gmail.com>
2022-02-22 14:36:38 +1100

    test

--- client/common/entities/projectile.js
+++ client/common/entities/projectile.js
@@ -1,32 +1,32 @@
 import { Entity } from './base-entity.js'
 import { ENTITY_CATEGORY } from '../constants.js'
 import radians from '../utils/radians.js';
+import Point from '../utils/point.js'
 
 export class Projectile extends Entity {
     category = ENTITY_CATEGORY.projectiles;
     objType = this.constructor.name;
 
-    constructor({x, y, size, damage, speed, lifetime}) {
+    constructor({x, y, size, projDesc}) {
         super(x, y, size)
         this.startX = x;
         this.startY = y; //used in determining where bullet is
         this.size = size;
-        this.damage = damage;
-        this.speed = speed;
-        this.lifetime = lifetime;
+        this.projDesc = projDesc
     }
 
-    tick(manager) {
-        const elapsedTime = (Date.now() - this.creationTs)/1000;
-
+    tick(manager, elapsedTime) {
         if (elapsedTime > this.lifetime) return false;
         if (this.detectEntityCollision(manager.player)) return false;
         for (const wallEntity of Object.values(manager.entities.walls)) {
             if (this.detectEntityCollision(wallEntity)) return false;
         }
-
-        this.x = this.startX + elapsedTime * this.speed * Math.cos(radians(this.direction));
-        this.y = this.startY + elapsedTime * this.speed * Math.sin(radians(this.direction));
         return true;
+    }
+
+    getPosition(elapsedTime) {
+        const x = this.startX + elapsedTime * this.speed * Math.cos(radians(this.direction));
+        const y = this.startY + elapsedTime * this.speed * Math.sin(radians(this.direction));
+        return new Point(x, y);
     }
 } 
