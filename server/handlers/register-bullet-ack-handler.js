810a99905dbe3d22d7b9225624056a36b82b5535
haha-XD <perfectionofthehighestorder@gmail.com>
2022-03-01 22:05:43 +1100

    bullet ticking/despawning

--- server/handlers/register-bullet-ack-handler.js
+++ server/handlers/register-bullet-ack-handler.js
@@ -13,18 +13,22 @@ export function registerBulletAckHandler(manager, socket) {
     const onTime = function(clientTime) {
         //COLLISION CHECK
         const p = socket.profile;
+        const world = manager.worlds[p.currentWorld];
         for (const bulletsObj of p.bullets) {
             const creationTs = bulletsObj.creationTs;
-            const bullets = bulletsObj.bullets;
+            let bullets = bulletsObj.bullets;
+            let rmvArray = []
             for (let bullet of bullets) {
                 const elapsedTime = clientTime - creationTs;
                 const pos = bullet.getPosition(elapsedTime)
                 bullet.x = pos.x;
                 bullet.y = pos.y;
+                if (!bullet.tick(world, elapsedTime)) rmvArray.push(bullet); 
                 if (bullet.detectEntityCollision(p.playerEntity)) {
                     p.playerEntity.hp -= bullet.damage;
                 }
             }
+            bulletsObj.bullets = bullets.filter(element => !rmvArray.includes(element))
         }
     }
 
