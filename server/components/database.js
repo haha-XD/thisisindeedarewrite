48fab58d2fe7a3fb964691d3e1fa6af9b0572727
haha-XD <perfectionofthehighestorder@gmail.com>
2022-03-05 14:19:22 +1100

    testdb

---
+++ server/components/database.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg')
+
+export class Database {
+    constructor() {
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+              rejectUnauthorized: false
+            }
+        })
+        this.db = {
+            query: (text, params, callback) => {
+                return pool.query(text, params, callback)
+            }
+        }
+    }
+
+    createAccountTable() {
+        this.db.query(
+            "CREATE TABLE accounts (" +
+                "user_id serial PRIMARY KEY," + 
+                "username VARCHAR ( 50 ) UNIQUE NOT NULL," +
+                "password VARCHAR ( 50 ) NOT NULL," +
+                "email VARCHAR ( 255 ) UNIQUE NOT NULL," +
+                "created_on TIMESTAMP NOT NULL," +
+                    "last_login TIMESTAMP " +
+            ");"
+        )
+    }
+}
